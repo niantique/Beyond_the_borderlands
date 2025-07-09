20f9c12bc619fb14130a6f4c193e0628e8431a00
niantique <nina.quetin@gmail.com>
2025-07-09 16:24:51 +0200

    login security

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,12 +5,21 @@ namespace App\DataFixtures;
 use App\Entity\Comment;
 use App\Entity\Stop;
 use App\Entity\Trip;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use DateTime;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    private const DATA_DIR = __DIR__ . '/data';
+    public function __construct(
+        private UserPasswordHasherInterface $hasher
+    ) {
+    }
+
+        private const DATA_DIR = __DIR__ . '/data';
+
     public function load(ObjectManager $manager): void
     {
         $fileContents = file_get_contents(self::DATA_DIR . '/user.json');
@@ -18,10 +27,30 @@ class AppFixtures extends Fixture
 
         $userMap = [];
         $id = 1;
+
+        $regularUser = new User();
+        $regularUser
+        ->setEmail('regular@user.com')
+        ->setUsername('regular')
+        ->setPicture("https://images.unsplash.com/photo-1611880147493-7542bdb0f024?q=80&w=1333&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+        ->setPassword($this->hasher->hashPassword($regularUser, 'test'));
+        $manager->persist($regularUser);
+        $userMap[$id++] = $regularUser;
+
+        $adminUser = new User();
+        $adminUser
+        ->setEmail('admin@user.com')
+        ->setUsername('admin')
+        ->setPicture("https://images.unsplash.com/photo-1487164697898-db7bfc2b7bf5?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+        ->setRoles(['ROLE_ADMIN'])
+        ->setPassword($this->hasher->hashPassword($adminUser, 'admin'));
+        $manager->persist($adminUser);
+        $userMap[$id++] = $adminUser;
+
         foreach ($userArray as $userJson) {
             $user = new User();
             $user->setUsername($userJson['username']);
-            $user->setPassword($userJson['password']);
+            $user->setPassword($this->hasher->hashPassword($user, $userJson['password']));
             $user->setEmail($userJson['email']);
             $user->setPicture($userJson['picture']);
             $manager->persist($user);
@@ -46,7 +75,8 @@ class AppFixtures extends Fixture
             $trip->setStartDate(new \DateTime($tripJson['startDate']));
             $trip->setEndDate(new \DateTime($tripJson['endDate']));
             $trip->setImage($tripJson['image']);
-            $trip->setUser($userMap[$tripJson['user']]);
+            $randomUserKey = array_rand($userMap);
+            $trip->setUser($userMap[$randomUserKey]);
             $manager->persist($trip);
             $tripMap[$id] = $trip;
             $id++;
